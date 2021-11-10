94bfe1e3391ed716c8270782e373c0435b4b50f0
Chiwende <chiwendesakala@gmail.com>
2021-11-10 10:55:17 +0200

    payload fix

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -9,8 +9,21 @@ export class PaymentService {
 
     async makePaymentrequest(payload: any){  
         console.log(payload)
+        const string = Date.now()
+        const prefix = "MPOS-"
+        const request_id = prefix+string
+        const request_payload = {
+            "msisdn": payload.msisdn,
+            "amount": payload.amount,
+            "firstName": "Chiwende",
+            "lastName": "Sakala",
+            "email": "chiwendesakala@gmail.com",
+            "requestId": request_id,
+            "reference": "Test Payment"
+        }
+        console.log(request_payload)
         const url = "https://lipila.hobbiton.tech/merchant/mobile/payments"
-        return await this.httpService.post(url,payload, {headers: {
+        return await this.httpService.post(url,request_payload, {headers: {
             'Content-Type': 'application/json',
             'ApiKey':'c38b58b1-affa-4aad-aec3-2c24e1132f05'
         }}).toPromise().then((response) => {
